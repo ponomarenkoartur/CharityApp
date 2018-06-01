3669cf5ea580c61317ffbe63e4e16ffb354c176f
Artur Ponomarenko <ponomarenkoartur@protonmail.com>
2018-06-01 20:58:05 +0300

    Add 'Donations' VC.

---
+++ CharityApp/ViewControllers/DonationsVC.swift
@@ -0,0 +1,20 @@
+//
+//  DonationsVC.swift
+//  CharityApp
+//
+//  Created by Artur on 6/1/18.
+//  Copyright © 2018 Artur. All rights reserved.
+//
+
+import UIKit
+
+class DonationsVC: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO: Add loading html to webpage
+    }
+}
