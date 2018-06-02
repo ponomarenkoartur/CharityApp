bedff38f455063d5e9410cc5bc930f7348c7cdcc
Artur Ponomarenko <ponomarenkoartur@protonmail.com>
2018-06-02 16:30:52 +0300

    Load webpage to 'DonationsVC'.

--- CharityApp/ViewControllers/DonationsVC.swift
+++ CharityApp/ViewControllers/DonationsVC.swift
@@ -9,9 +9,41 @@
 import UIKit
 
 class DonationsVC: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    // MARK: - Properties
+    
+    var dataTask: URLSessionDataTask?
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        // TODO: Add loading html to webpage
+        if let url = URL(string: "https://ib.fio.cz/ib/transparent?a=2300849910&l=ENGLISH") {
+            let urlRequest = URLRequest(url: url)
+            webView.loadRequest(urlRequest)
+            
+        }
+        
+//        let myURLString = "https://ib.fio.cz/ib/transparent?a=2300849910&l=ENGLISH"
+//        guard let myURL = URL(string: myURLString) else {
+//            print("Error: \(myURLString) doesn't seem to be a valid URL")
+//            return
+//        }
+//
+//        do {
+//            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+//            print("HTML : \(myHTMLString)")
+//        } catch let error {
+//            print("Error: \(error)")
+//        }
     }
 }
